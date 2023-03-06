2a33a76b359dd4f540abf76029d6aef3c7e4fbd7
Alexandre <alexandrepp07@ua.pt>
2023-03-06 22:40:16 +0000

    merge sort single thread done

--- labs/lab1/mergeSorting.c
+++ labs/lab1/mergeSorting.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 
+    The merge function takes in an array arr and three indices l, m, and r. 
+  It assumes that the two subarrays arr[l..m] and arr[m+1..r] are already sorted, 
+  and it merges them into a single sorted array arr[l..r]
+*/
 void merge(int arr[], int l, int m, int r) {
     int i, j, k;
+
+    // size of first array
     int n1 = m - l + 1;
+
+    // size of second array
     int n2 = r - m;
 
+    // create temporary arrays you the size of the arrays
     int L[n1], R[n2];
 
+    // values are copied into the arrays
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
@@ -17,6 +28,7 @@ void merge(int arr[], int l, int m, int r) {
     j = 0;
     k = l;
 
+    // iterates through the two subarrays, comparing the values at each index and inserting them into the correct position in the final sorted array arr
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -29,6 +41,7 @@ void merge(int arr[], int l, int m, int r) {
         k++;
     }
 
+    // any remaining elements in L or R are copied over to the final sorted array
     while (i < n1) {
         arr[k] = L[i];
         i++;
@@ -42,6 +55,11 @@ void merge(int arr[], int l, int m, int r) {
     }
 }
 
+
+/*
+    The mergeSort function takes in an array arr and two indices l and r. 
+  It recursively splits the array into smaller arrays and then merges them back together using the merge function.
+*/
 void mergeSort(int arr[], int l, int r) {
     if (l < r) {
 
@@ -59,39 +77,36 @@ void mergeSort(int arr[], int l, int r) {
     }
 }
 
+
+
 int main(int argc, char *argv[]) {
-    
-    // how to user this script
+
+    // how to use this script
     if (argc != 2) {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
+    // open binary file
     FILE *fp;
-    fp = fopen(argv[1], "r");
+    fp = fopen(argv[1], "rb");
 
+    // when unable to open file
     if (fp == NULL) {
         printf("Unable to open file %s\n", argv[1]);
         return 1;
     }
 
-    int num;
-    int count = 0;
-    // count total number of integers
-    while (fscanf(fp, "%d", &num) != EOF) {
-        count++;
-    }
-
+    // count the number of integers
+    fseek(fp, 0, SEEK_END);
+    long count = ftell(fp) / sizeof(int);
     fseek(fp, 0, SEEK_SET);
 
     int arr[count];
-    int i = 0;
-    // store the integers in an array
-    while (fscanf(fp, "%d", &num) != EOF) {
-        arr[i] = num;
-        i++;
-    }
+    // reads one integer at a time from the binary file
+    fread(arr, sizeof(int), count, fp);
 
+    // close binary file
     fclose(fp);
 
     // merge sort
@@ -99,10 +114,11 @@ int main(int argc, char *argv[]) {
 
     // print sorted array
     printf("Sorted array: ");
-    for (i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
     return 0;
 }
+
